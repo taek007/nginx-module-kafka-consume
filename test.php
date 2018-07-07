53f9ed53bd30db26617e6b4550c4818f4a638cd1
taek007 <taek-007@163.com>
2018-07-08 00:44:08 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$url = "http://10.99.1.148:8029/consume?group_name=ab";
+$url = "http://10.99.1.148:8029/consume?group_name=test_group";
 while(1){
 	$res = file_get_contents($url);
 	echo $res."\n";
